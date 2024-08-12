cdc05ac92bd6a395f83fc5f67c29229f047731a9
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2024-08-12 18:32:56 -0500

    carrusel responsive

--- src/components/carrusel1/carru.jsx
+++ src/components/carrusel1/carru.jsx
@@ -1,16 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './carru.css';
 
 function CarruselOne() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const posters = [
+  const [posters, setPosters] = useState([
     "./src/img/bannerA1.webp",
     "./src/img/bannerB1.webp",
     "./src/img/bannerC1.webp",
     "./src/img/bannerD1.webp",
     "./src/img/bannerE1.webp",
     "./src/img/bannerF1.webp",
-  ];
+  ]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 932) {
+        setPosters([
+          "./src/img/bannerA2.webp",
+          "./src/img/bannerB2.webp",
+          "./src/img/bannerC2.webp",
+          "./src/img/bannerD2.webp",
+          "./src/img/bannerE2.webp",
+          "./src/img/bannerF2.webp",
+        ]);
+      } else {
+        setPosters([
+          "./src/img/bannerA1.webp",
+          "./src/img/bannerB1.webp",
+          "./src/img/bannerC1.webp",
+          "./src/img/bannerD1.webp",
+          "./src/img/bannerE1.webp",
+          "./src/img/bannerF1.webp",
+        ]);
+      }
+    };
+
+    // Set initial images based on window size
+    handleResize();
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const handlePrev = () => {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? posters.length - 1 : prevIndex - 1));
