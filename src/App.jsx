b36b653eb43bd42b2859b6546cc11586463551ac
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2024-08-14 16:44:32 -0500

    img productos

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from "react"
 import BarraNav from "./components/barra/barra"
 import CarruselOne from "./components/carrusel1/carru"
 import FotosCont from "./components/contenedores/fotos"
+import Productos from "./components/caru-produ/carupro"
 function App() {
   return (
    <>
    <BarraNav/>
    <CarruselOne/>
    <center><FotosCont/></center>
+   <Productos/>
    </>
   )
 }
---
+++ src/components/caru-produ/carupro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './carupro.css';
+
+function Productos() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default Productos;
